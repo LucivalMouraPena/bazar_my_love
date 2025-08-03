3f1f270e59eac9f2c741c86c9045c86e57e2b7f7
lucivalmourapena <alunoeternoestudante@gmail.com>
2025-08-03 20:45:15 -0300

    Adicionar carrinho de compras funcional

--- carrinho/urls.py
+++ carrinho/urls.py
@@ -3,9 +3,10 @@ from django.urls import path
 from bazar import views as bazar_views
 from . import views
 
+from django.urls import include, path
+
 urlpatterns = [
-    path('', bazar_views.home, name='home'),
-    path('produtos/', bazar_views.produtos, name='produtos'),
-    path('adicionar/<int:produto_id>/', views.adicionar_ao_carrinho, name='adicionar_ao_carrinho'),
-    path('ver/', views.ver_carrinho, name='ver_carrinho'),
+    path('', include('bazar.urls')),
+    path('carrinho/', include('carrinho.urls')),  # <-- essa linha
+    path('admin/', admin.site.urls),
 ]
--- config/urls.py
+++ config/urls.py
@@ -12,20 +12,28 @@ Class-based views
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+    2. Add a URL to urlpatt erns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path, include 
-#from bazar import views
-from bazar import views # CERTO
+from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
+from bazar import views as bazar_views
+from carrinho import views as carrinho_views  # Se tiver uma app 'carrinho'
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.home, name='home'),
-    path('produtos/', views.produtos, name='produtos'),
+    path('admin/', admin.site.urls),  # painel admin
+
+    path('', bazar_views.home, name='home'),  # página principal
+    path('produtos/', bazar_views.home, name='produtos'),
+
+
+    path('carrinho/produtos/', bazar_views.carrinho_produtos, name='carrinho_produtos'),
+    path('adicionar/<int:produto_id>/', bazar_views.adicionar_ao_carrinho, name='adicionar_ao_carrinho'),
+    path('remover/<int:produto_id>/', bazar_views.remover_do_carrinho, name='remover_do_carrinho'),
+    path('atualizar/', bazar_views.atualizar_carrinho, name='atualizar_carrinho'),
 ]
 
 if settings.DEBUG:
